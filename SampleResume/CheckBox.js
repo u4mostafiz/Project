d4fcb5467e6e75307b7dc6dc1b294e510e595a2d
u4mostafiz <u4mostafiz@gmail.com>
2015-08-20 14:59:07 -0700

    HomeWork
    homework

---
+++ SampleResume/CheckBox.js
@@ -0,0 +1,18 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!-- 
+
+<!-- Begin
+function Check(chk)
+{
+if(document.myform.Check_ctr.checked==true){
+for (i = 0; i < chk.length; i++)
+chk[i].checked = true ;
+}else{
+
+for (i = 0; i < chk.length; i++)
+chk[i].checked = false ;
+}
+}
+
+// End -->
+</script>
